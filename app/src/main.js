877917f07a2c4e593eb27ff15b16f4eebc252ff2
zaehicks <isaiahhickerson@gmail.com>
2024-01-22 14:49:48 -0500

    added helper function in utils

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,9 @@
 import './style.css'
+import fetchData from './utils.js'
 
+
+
+console.log( await fetchData("https://pokeapi.co/api/v2/pokemon/ditto"))
 
 document.querySelector('#app').innerHTML = `
 <p>Hello Pokemon </p>`
