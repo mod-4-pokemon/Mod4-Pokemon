670955cbf46b3158d9fe1aae8943f59db7c6106d
Aaron C <aaronc@MacBook-Pro-6.local>
2024-01-24 15:01:35 -0500

    modal

--- app/src/main.js
+++ app/src/main.js
@@ -3,17 +3,15 @@ import { selectPokemon } from './selectPokemon.js'
 import { renderPoke } from "./render.js";
 //import name of selected pokemon
 
-selectPokemon('pikachu')
 // console.log( await fetchData("https://pokeapi.co/api/v2/pokemon/"))
 
 document.querySelector('#app').innerHTML = `
 <p>Hello Pokemon </p>`
 
-
-
 const main = () => {
-  renderPoke();
+    renderPoke();
 };
 
-main();
+main()
+
 
--- app/src/selectPokemon.js
+++ app/src/selectPokemon.js
@@ -1,29 +1,52 @@
 import fetchData from "./utils";
 
+
+
+let modal = document.createElement('div')
+let modalContent = document.createElement('div')
+modal.id = 'modal'
+modalContent.className = 'modal-content'
+
+
+
 export const selectPokemon = async (pokeName) => {
     const pokemon = await fetchData(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
 
     const { abilities, moves, order, types, weight, sprites } = pokemon[0]
 
-    const img = document.createElement('img')
-    const abilityInfo = document.createElement('p')
-    const movesInfo = document.createElement('p')
-    const orderInfo = document.createElement('p')
-    const typeInfo = document.createElement('p')
-    const weightInfo = document.createElement('p')
+    // const img = document.createElement('img')
+    // const abilityInfo = document.createElement('p')
+    // const movesInfo = document.createElement('p')
+    // const orderInfo = document.createElement('p')
+    // const typeInfo = document.createElement('p')
+    // const weightInfo = document.createElement('p')
+    
 
-    img.src = sprites.front_default
-    abilityInfo.textContent = `Ability: ${abilities[0].ability.name}`
-    movesInfo.textContent = `Moves: ${moves.slice(0,3).map(x => x.move.name)}`
-    orderInfo.textContent = `Pokemon #${order}`
-    typeInfo.textContent = `Type: ${types[0].type.name}`
-    weightInfo.textContent = `Weight: ${weight}lbs`
-    console.log(weightInfo)
 
-    let modal = document.createElement('div')
-    let modalContent = document.createElement('div')
-    modal.id = 'modal'
-    modalContent.className = 'modal-content'
-    modalContent.append(img, abilityInfo, movesInfo, orderInfo, typeInfo, weightInfo)
+
+
+    // img.src = sprites.front_default
+    // abilityInfo.textContent = `Ability: ${abilities[0].ability.name}`
+    // movesInfo.textContent = `Moves: ${moves.slice(0,3).map(x => x.move.name)}`
+    // orderInfo.textContent = `Pokemon #${order}`
+    // typeInfo.textContent = `Type: ${types[0].type.name}`
+    // weightInfo.textContent = `Weight: ${weight}lbs`
+
+
+   
+ 
     modal.append(modalContent)
+    const container = document.getElementById("pokeContainer")
+    container.prepend(modal)
+
+
+    modalContent.innerHTML = `
+    <img src=${sprites.front_default} />
+    <p>Ability: ${abilities[0].ability.name}</p>
+    <p>Moves: ${moves.slice(0, 3).map(x => x.move.name).join(', ')}</p>
+    <p>Pokemon #${order}</p>
+    <p>Type: ${types[0].type.name}</p>
+    <p>Weight: ${weight} lbs</p>
+  `;
+  
 };
